5d8c3968ec8bee5020cab18f1b7cf857cf6d931f
Zeo-shark <sauravberaiit@gmail.com>
2020-12-18 12:55:12 +0530

    add dateToMinute

--- weather_prediction.py
+++ weather_prediction.py
@@ -39,3 +39,45 @@ def print_data_type(x):
     for f in x.columns:
         print("f = {}".format(f))
         print(x[f].dtype)
+
+def RepresentsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def RepresentsFloat(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def dateToMinute(s, option):
+    """
+        time delta from the beginning of that year, unit = minutes
+        s format = '%Y-%m-%d %H:%M'
+        return type: int
+        """
+    if (isinstance(s, str)):
+        time_obj= datetime.strptime(s, "%Y-%m-%d %H:%M")
+    elif (isinstance(s, datetime)):
+        time_obj= datetime.strptime( s.strftime('%Y-%m-%d %H:%M'),"%Y-%m-%d %H:%M" )
+    else:
+        raise SystemError("input is not a valid string/datetime obj!")
+
+    if(option == 'year'):
+        time_diff= time_obj - datetime(time_obj.year, 0o1, 0o1, 0, 0)
+    elif(option== 'month'):
+        time_diff= time_obj - datetime(time_obj.year, time_obj.month, 0o1, 0, 0)
+    elif(option== 'day'):
+        time_diff= time_obj - datetime(time_obj.year, time_obj.month, time_obj.day, 0,0)
+    elif(option == 'hour'):
+        time_diff= time_obj - datetime(time_obj.year, time_obj.month, time_obj.day,time_obj.hour, 0)
+    else:
+        raise SystemError("Option is not a valid string !")
+
+    return int(time_diff.total_seconds()/60)
+
+#
